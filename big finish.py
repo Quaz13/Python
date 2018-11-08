b47541a01923ffc3064f6be11c9e1990dee2c36e
Quaz13 <44881754+Quaz13@users.noreply.github.com>
2018-11-08 18:52:52 -0300

    demo

---
+++ big finish.py
@@ -0,0 +1,9 @@
+#Cody, 11/6/18, bootcamp1
+print ('what is your height?')
+height = input()
+print(height)
+bootcamps = ['deandre', 'ayanna', 'ruben', 'E.J', 'johnny', 'marty']
+for bootcamp in bootcamps:
+    print(bootcamp)
+if height == '55':
+    print('finished')
